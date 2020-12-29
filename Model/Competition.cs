a5b711877fb5c254a645efca217b291b40a75698
Jacco <jjveldscholten@gmail.com>
2020-12-29 13:11:48 +0100

    Added goose points

--- Model/Competition.cs
+++ Model/Competition.cs
@@ -6,7 +6,7 @@ namespace Model {
     public class Competition {
         public List<IParticipant> Participants { get; set; }
         public Queue<Track> Tracks { get; set; }
-        public RaceStats<Points> RaceStatPoints;
+        public RaceStats<GoosePoints> RaceStatPoints;
         public RaceStats<SectionRoundtime> RaceRoundTime;
         public RaceStats<SectionSpeed> RaceSectionSpeed;
         public RaceStats<LostWings> brokeCounter;
@@ -15,7 +15,7 @@ namespace Model {
         public Competition() {                                          // make new competition
             Participants = new List<IParticipant>();
             Tracks = new Queue<Track>();
-            RaceStatPoints = new RaceStats<Points>();
+            RaceStatPoints = new RaceStats<GoosePoints>();
             RaceRoundTime = new RaceStats<SectionRoundtime>();
         }
         public Track NextTrack() {                                      // get next track, dequeue current
@@ -31,7 +31,7 @@ namespace Model {
         public void GivePoints(List<IParticipant> order) {              // give participating participants points, given through the order
             int mP = maxPoints;
             for (int i = 0; i < order.Count; i++) {
-                RaceStatPoints.addRaceStatToList(new Points() { name = order[i].Name, points = mP });
+                RaceStatPoints.addRaceStatToList(new GoosePoints() { name = order[i].Name, Points = mP });
                 if (mP > 2) {
                     mP -= 2;
                 }
---
+++ Model/GoosePoints.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model {
+    public class GoosePoints : IGenericScope {
+        public string name { get; set; }
+        public int Points { get; set; }
+
+        void IGenericScope.Add<T>(List<T> list) {  
+            // Add points to the goose
+            foreach (var goose in list) {
+                var goosePoints = goose as GoosePoints;
+                if (goose.name == name) {
+                    goosePoints.Points += Points;
+                    return;
+                }
+            }
+            list.Add(this as T);
+        }
+
+        public string GetBest<T>(List<T> list) where T : class, IGenericScope {      
+            // Get the goose with highest score
+            int best = 0;
+            GoosePoints goosePointsObj = this;
+            foreach (var goose in list) {
+                var goosePoints = goose as GoosePoints;
+
+                if (goosePoints.Points > best) {
+                    best = goosePoints.Points;
+                    goosePointsObj = goosePoints;
+                }
+            }
+            return goosePointsObj.name;
+        }
+    }
+}
